ccba766fa8051c7e3cc2d6eb0242ba551eef88e5
hasanisu <hasanjimam@gmail.com>
2023-05-15 17:46:23 +0900

    added news and category

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useLoaderData} from 'react-router-dom';
 
 const Category = () => {
+    const news = useLoaderData();
     return (
         <div>
-            <h3>This is category</h3>
+            <h3>This is category wise data: {news.length}</h3>
         </div>
     );
 };
