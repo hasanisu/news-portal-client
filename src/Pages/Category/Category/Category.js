f5cf7a55a9e28ea0cbeaee4e6d9d92d4c1748794
Hasan Imam <hasanjimam@gmail.com>
2023-05-15 23:19:00 +0900

    add context

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {useLoaderData} from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Category = () => {
-    const news = useLoaderData();
+    const categoryNews = useLoaderData();
     return (
         <div>
-            <h3>This is category wise data: {news.length}</h3>
+            <h3>This is category wise data: {categoryNews.length}</h3>
+
+            {
+                categoryNews.map(news => <NewsSummaryCard
+                key={news._id}
+                news={news}
+                ></NewsSummaryCard>)
+            }
         </div>
     );
 };
---
+++ src/_steps.js
@@ -0,0 +1,17 @@
+/*
+Step by step coding
+1. prothome ekta application install koreci
+2. ekta sever ready ready koreci
+3. besh koiekta component ready koreci
+4. and router ready koreci
+
+api connection 
+
+1. server theke prothome ekta leftside news category niye esheci
+2. server e kisu news er data niye sheigulo k api link e convert koreci
+3. category route e giye api er maddhome data load koreci and data gulo ke dekhiyesi
+ 
+
+
+
+*/
